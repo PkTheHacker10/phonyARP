858eaf10407a8cdf1019fc7a261eb5db7a980f9c
pk <pk_server@pkserver@quickcom.com>
2024-12-15 07:34:06 +0000

    help added

---
+++ phonyARP/modules/help/help.py
@@ -0,0 +1,30 @@
+from colorama import Fore,Style 
+
+red=Fore.RED
+blue=Fore.BLUE
+white=Fore.WHITE
+magenta=Fore.MAGENTA
+yellow=Fore.YELLOW
+green=Fore.GREEN
+cyan=Fore.CYAN
+bold=Style.BRIGHT
+reset=Style.RESET_ALL
+
+def help():
+    help=f"""\n
+    {bold}{white}[{reset}{bold}{blue}DESCRIPTION{reset}{white}]{reset}: {white}{bold}PhonyArp{reset} {white}is a tool which is purely meant for ARPSpoofing by {reset}{bold}{green}PkTheHacker10{reset}.\n
+    {bold}{white}[{reset}{bold}{blue}Usage{reset}{white}]{reset}:\n
+            {white}phonyarp {bold}{white}[{reset}{bold}{blue}flags{reset}{bold}{white}]\n
+    [{reset}{bold}{blue}flags{reset}{bold}{white}]
+                
+            [{reset}{bold}{blue}input{reset}{bold}{white}]{reset}
+                   
+                -t,   --target                  :  target ip to spoof the arp table.
+                -g,   --gateway                 :  gatway ip to imprisonate to the target. 
+                    
+            {bold}{white}[{reset}{bold}{blue}debug{reset}{bold}{white}]{reset}
+                   
+                -v,   --version                 :  to check version of this tool. 
+                -h,   --help                    :  to see all the available options.
+          """
+    return help
--- phonyARP/phonyarp.py
+++ phonyARP/phonyarp.py
@@ -1,10 +1,27 @@
-from phonyARP.modules.banner import banner
-from phonyARP.modules.cli import cli
+import sys
+from colorama import Fore,Style 
 
+blue=Fore.BLUE
+bright=Style.BRIGHT
+reset=Style.RESET_ALL
+
+try:
+    from phonyARP.modules.banner import banner
+    from phonyARP.modules.cli import cli
+    from phonyARP.modules.help import help
+    
+except ImportError as e:
+    print(f"{bright}{blue}INFO:{reset}Could't import :{e}")
+    sys.exit()
+    
 class handler():
+    
     def __init__(self):
         self.toolbanner=banner.banner()
         self.args=cli.args()
+        self.help=help.help()
+        
     def start(self):
         print(self.toolbanner)
         print(self.args)
+        print(self.help)
